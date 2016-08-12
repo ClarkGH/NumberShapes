b6e755a15c7e81dabdab524780b55381dee5f59a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-11 21:34:12 -0700

    logs fine

--- app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.clarkhinchcliff.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void testNumber(View view) {
+
+        EditText userNum = (EditText) findViewById(R.id.editText);
+
+        Log.i("users number", userNum.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
