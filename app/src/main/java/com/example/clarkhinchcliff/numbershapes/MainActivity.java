5b737fb7d526e95806547a3b8072676a52218e74
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-13 18:21:14 -0700

    Image Fade animation fun

--- app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.clarkhinchcliff.numbershapes;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+
+        ImageView morty = (ImageView) findViewById(R.id.morty);
+
+        morty.animate().alpha(0f).setDuration(500);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
