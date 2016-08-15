a836b515980c7098430edf0f651429ed833ba75a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-14 18:13:13 -0700

    Rotates now

--- app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/numbershapes/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     public void fade(View view) {
 
         ImageView morty = (ImageView) findViewById(R.id.morty);
-        morty.animate().translationYBy(1000f).setDuration(2000);
+        morty.animate().rotation(180f).setDuration(2000);
 
         ImageView rick = (ImageView) findViewById(R.id.rick);
 //        rick.animate().alpha(1f).setDuration(500);
@@ -22,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
         ImageView morty = (ImageView) findViewById(R.id.morty);
         morty.setTranslationX(-1000f);
+        */
     }
 }
